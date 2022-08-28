96e75fc87b77b7ed47865fbd445b44ff34ff6576
Jonathan Menschaert <jonathan.658@telenet.be>
2022-08-28 16:28:08 +0200

    Image slider improved

--- PortfolioScripts.js
+++ PortfolioScripts.js
@@ -8,3 +8,18 @@ function switchNavBar()
         x.className = "navbar";
     }
 }
+
+var counter = 1;
+function adjustCounter(newCounter) {
+    if (newCounter > 4) {
+        newCounter = 1;
+    }
+    document.getElementById("radionav" + counter).className = "manual-btn";    
+    counter = newCounter;
+    document.getElementById("radionav" + counter).className = "manual-btn manual-btn-selected";
+    document.getElementById("radio" + counter).checked = "checked";
+}
+
+setInterval(function(){
+    adjustCounter(counter + 1);
+}, 5000);
