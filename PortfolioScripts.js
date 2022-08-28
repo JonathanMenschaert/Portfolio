5e27d2fb5c1a32f6cf73e791835d2e2e399cb9b6
Jonathan Menschaert <jonathan.658@telenet.be>
2022-08-28 22:22:23 +0200

    Added slider description and title placeholders

--- PortfolioScripts.js
+++ PortfolioScripts.js
@@ -1,5 +1,4 @@
-function switchNavBar()
-{
+function switchNavBar() {
     var x = document.getElementById("NavBar");
     if (x.className == "navbar") {
         x.className += " expand";
@@ -10,16 +9,33 @@ function switchNavBar()
 }
 
 var counter = 1;
-function adjustCounter(newCounter) {
+var scroll = true;
+function adjustCounter(newCounter, shouldScroll = true) {
+
     if (newCounter > 4) {
         newCounter = 1;
     }
-    document.getElementById("radionav" + counter).className = "manual-btn";    
+    document.getElementById("radionav" + counter).className = "manual-btn";
     counter = newCounter;
     document.getElementById("radionav" + counter).className = "manual-btn manual-btn-selected";
     document.getElementById("radio" + counter).checked = "checked";
+
+    if (!shouldScroll) {
+        scroll = false;
+    }
 }
 
-setInterval(function(){
-    adjustCounter(counter + 1);
-}, 5000);
+setInterval(function () {
+    if (scroll) {
+        if (counter >= 4) {
+            counter = 1;
+        }
+        else {
+            counter++;
+        }
+        document.getElementById("radionav" + counter).className = "manual-btn";
+        counter = newCounter;
+        document.getElementById("radionav" + counter).className = "manual-btn manual-btn-selected";
+        document.getElementById("radio" + counter).checked = "checked";
+    }
+}, 7000);
