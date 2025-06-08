c23c58c247b0d90f427df271c085957b60d94706
Jonathan Menschaert <jonathan.658@telenet.be>
2025-06-09 00:15:25 +0200

    Made navbar more responsive
    - Added new expand and collapse animation

--- scripts/GeneralScripts.js
+++ scripts/GeneralScripts.js
@@ -1,10 +1,16 @@
-function activateNavBar(activate) 
+function activateNavbar(activate) 
 {
     let x = document.getElementById("NavBar");
     if (activate) {
-        x.className += " expand";
+        x.className = "navbar expand";
     }
     else {
-        x.className = "navbar";
+        x.className = "navbar collapse";
     }
+}
+
+function resetNavbar()
+{
+    let x = document.getElementById("NavBar");
+    x.className = "navbar";
 }
