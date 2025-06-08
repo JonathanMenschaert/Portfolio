33287e91ef7fafb0331738eff8488042681b78a2
Jonathan Menschaert <jonathan.658@telenet.be>
2025-06-08 22:19:22 +0200

    Improved highlight control panel
    - added control panel

--- scripts/MainPageScripts.js
+++ scripts/MainPageScripts.js
@@ -1,20 +1,28 @@
-var counter = 1;
+var counter = 0;
 var scroll = true;
 var maxImages = 4;
 
 function adjustCounter(newCounter) 
 {
-    if (newCounter > maxImages) {
-        newCounter = 1;
-    }
-    document.getElementById("radionav" + counter).className = "manual-btn";
-    counter = newCounter;
-    document.getElementById("radionav" + counter).className = "manual-btn manual-btn-selected";
-    document.getElementById("radio" + counter).checked = "checked";
+    counter = counter % maxImages;
+    document.getElementById("radionav" + (counter + 1)).className = "manual-btn";
+    counter = newCounter % maxImages;
+    document.getElementById("radionav" + (counter + 1)).className = "manual-btn manual-btn-selected";
+    document.getElementById("radio" + (counter + 1)).checked = "checked";
 }
 
 function adjustManual(newCounter) 
 {   
     adjustCounter(newCounter);    
     scroll = false;    
+}
+
+function increaseCounter()
+{   
+    adjustCounter(counter + 1);    
+}
+
+function decreaseCounter()
+{    
+    adjustCounter(counter + maxImages - 1);    
 }
